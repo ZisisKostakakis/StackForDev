3423f0849c421a6172c8ad2f0c11e42d36728cb0
ZisisKostakakis <dev@zisiskostakakis.com>
2026-02-22 21:01:49 +0000

    fix: import existing CloudWatch log group into Terraform state

--- aws_resources/cloudwatch.tf
+++ aws_resources/cloudwatch.tf
@@ -1,3 +1,8 @@
+import {
+  to = aws_cloudwatch_log_group.lambda
+  id = "/aws/lambda/stack-for-dev"
+}
+
 # CloudWatch Log Group with 30-day retention
 resource "aws_cloudwatch_log_group" "lambda" {
   name              = "/aws/lambda/${aws_lambda_function.stack_for_dev.function_name}"
